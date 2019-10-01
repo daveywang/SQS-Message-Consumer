64cd9826490bd43682a947ff1228f72eadca0403
Liwei Wang <daveywang@live.com>
2019-10-01 14:36:12 -0400

    Init commit

---
+++ src/main/java/com/ascending/SQSMessageConsumer.java
@@ -0,0 +1,49 @@
+/*
+ *  Copyright 2019, Liwei Wang <daveywang@live.com>.
+ *  All rights reserved.
+ *  Author: Liwei Wang
+ *  Date: 06/2019
+ */
+
+package com.ascending;
+
+import com.amazon.sqs.javamessaging.ProviderConfiguration;
+import com.amazon.sqs.javamessaging.SQSConnection;
+import com.amazon.sqs.javamessaging.SQSConnectionFactory;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jms.*;
+import java.io.IOException;
+
+public class SQSMessageConsumer {
+    private static final String queueName = "training_queue_ascending_com";
+    private static Logger logger = LoggerFactory.getLogger(SQSMessageConsumer.class);
+
+    public static void main(String[] args) throws JMSException, IOException {
+        logger.info("The message consumer is listening... press ENTER to exit the application.");
+
+        // Create the connection factory
+        SQSConnectionFactory connectionFactory = new SQSConnectionFactory(new ProviderConfiguration(),
+                                                                          AmazonSQSClientBuilder.standard()
+                                                                          .withCredentials(new DefaultAWSCredentialsProviderChain())
+                                                                          .withRegion(Regions.US_EAST_2)
+        );
+
+        //Create the connection
+        SQSConnection connection = connectionFactory.createConnection();
+        //Create the session
+        Session session = connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+        //Create the consumer
+        javax.jms.MessageConsumer consumer = session.createConsumer(session.createQueue(queueName));
+        SQSMessageListener messageListener = new SQSMessageListener();
+        consumer.setMessageListener(messageListener);
+        connection.start();
+        System.in.read();
+        connection.close();
+        logger.info( "The Connection closed." );
+    }
+}
---
+++ src/main/java/com/ascending/SQSMessageListener.java
@@ -0,0 +1,49 @@
+/*
+ *  Copyright 2019, Liwei Wang <daveywang@live.com>.
+ *  All rights reserved.
+ *  Author: Liwei Wang
+ *  Date: 06/2019
+ */
+
+package com.ascending;
+
+import com.amazonaws.util.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jms.*;
+
+public class SQSMessageListener implements MessageListener {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public void onMessage(Message message) {
+        try {
+            handleMessage(message);
+            message.acknowledge();
+            logger.info("Acknowledged message " + message.getJMSMessageID());
+        }
+        catch (JMSException e) {
+            logger.error("Error processing message: " + e.getMessage());
+        }
+    }
+
+    private void handleMessage(Message message) throws JMSException {
+        logger.info("Got message " + message.getJMSMessageID());
+
+        if (message instanceof TextMessage) {
+            TextMessage txtMessage = (TextMessage)message;
+            logger.info("Content: " + txtMessage.getText());
+        }
+        else if (message instanceof BytesMessage){
+            BytesMessage byteMessage = (BytesMessage)message;
+            byte[] bytes = new byte[(int)byteMessage.getBodyLength()];
+            byteMessage.readBytes(bytes);
+            logger.info("Content: " +  Base64.encodeAsString(bytes));
+        }
+        else if (message instanceof ObjectMessage) {
+            ObjectMessage objMessage = (ObjectMessage) message;
+            logger.info("Content: " + objMessage.getObject());
+        }
+    }
+}
